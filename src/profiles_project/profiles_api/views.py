b0ec04ceff524325231da4932d47b2daf4dd2618
Khatib Omar <khatibomar3@gmail.com>
2020-03-23 22:48:45 +0100

    Added login API (but it's not working due to authorization problem

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -2,20 +2,21 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
-from rest_framework.views import APIView
-
-from rest_framework.response import Response
-
-from  . import serializers
 from rest_framework import status
 from rest_framework import viewsets
-from . import models
+from rest_framework.views import APIView
 from rest_framework.authentication import TokenAuthentication
-
-from . import permissions
 from rest_framework import filters
+from rest_framework.response import Response
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+
+from  . import serializers
+from . import models
+from . import permissions
+
 # Create your views here.
+
 
 class HelloApiView(APIView):
 
@@ -121,3 +122,13 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes=(permissions.UpdateOwnProfile,)
     filter_backends=(filters.SearchFilter,)
     search_fields=('name','email',)
+
+
+class LoginViewSet(viewsets.ViewSet):
+    """Checks email and password and returns an auth token """
+    serializer_class=AuthTokenSerializer
+
+    def create(self,request):
+        """Use the ObtainAuthToken APIView to validate and create and create a token"""
+
+        return ObtainAuthToken().post(request)
