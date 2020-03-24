b083fd3b69df96528027eb33cacabd9d98c0cc08
Khatib Omar <khatibomar3@gmail.com>
2020-03-24 04:15:34 +0100

    Some new fixes

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -6,10 +6,10 @@ from rest_framework.routers import DefaultRouter
 
 
 router=DefaultRouter()
-router.register('hello-viewset',views.HelloViewSet,base_name='hello-viewset')
+router.register('hello-viewset',views.HelloViewSet,basename='hello-viewset')
 router.register('profile',views.UserProfileViewSet)
-router.register('login',views.LoginViewSet,base_name='login')
-router.register('feed',views.UserProfileFeedViewSet)
+router.register('login',views.LoginViewSet,basename='login')
+router.register('feed',views.User)
 
 urlpatterns=[
     url(r'^hello-view/', views.HelloApiView.as_view()),
